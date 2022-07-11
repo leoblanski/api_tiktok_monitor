2315d755d558f06a8718b8e58d38de067f86b633
Leonardo Blanski <leo_blanski@hotmail.com>
2022-07-10 21:07:13 -0300

    const creating

--- app/Models/Moviment.php
+++ app/Models/Moviment.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Moviment extends Model
 {   
     protected $table = 'moviment';
+
+    const typeSponsor = '1';
+    const typeVoutes = '2';
+
+    const firstItem = '/lula';
+    const secondItem = '/bolsonaro';
 }
