056098c46f3629dc069cdede57ebc6d216665797
Leonardo Blanski <leo_blanski@hotmail.com>
2022-07-10 21:07:53 -0300

    list component implementation

--- resources/js/components/List.vue
+++ resources/js/components/List.vue
@@ -1,21 +1,25 @@
 <template>
-    <div >
-      <div class="">
-        <div class="list-header">
-          Patrocinadores da Live
-        </div>
-        <div class="list-body">
-          <tr v-for="item in list" :key="item.id">
-            <div class="col-md-4 list-tile">
-              <img class="rounded-circle shadow-lg p-2" v-bind:src="item.url_picture" data-holder-rendered="true">
-            </div>
-            <td>{{item.username}}</td>
-          </tr>
-        </div>
-        <div class="list-header"></div>
+  <div>
+    <div class="list-header">{{title}}</div>
+
+    <div class="container-list">
+      <div class="list-body">
+        <tr v-for="item in list" :key="item.id">
+          <div class="col-md-4 list-tile justify-content-end">
+            <img
+              class="rounded-circle shadow-lg p-2"
+              v-bind:src="item.url_picture"
+              data-holder-rendered="true"
+            />
+          </div>
+          <td>
+            <h3>{{ item.username }}</h3>
+          </td>
+        </tr>
       </div>
-      
+      <div class="list-header"></div>
     </div>
+  </div>
 </template>
 
 <style>
@@ -23,16 +27,23 @@
   margin-top: 10px;
 }
 
-.list-tile img{
+.container-list {
+  height: 200px;
+}
+
+.list-tile img {
   width: 80px;
   height: 80px;
 }
-
 </style>
 
 <script>
 export default {
   props: {
+    title: {
+      type: String,
+      required: true,
+    },
     list: {
       type: Array,
       required: true,
