9d5210ca347b7130440edf87f6611e6248c67b8c
Leonardo Blanski <leo_blanski@hotmail.com>
2022-07-10 21:07:02 -0300

    implementing type filters to get the result in differents locals

--- app/Http/Controllers/MovimentController.php
+++ app/Http/Controllers/MovimentController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 use Exception;
 use Illuminate\Http\Request;
@@ -23,6 +24,7 @@ class MovimentController extends Controller
             $moviment->qty_gift = $params['qty_gift'];
             $moviment->amount = $params['amount'];
             $moviment->type = $params['type'];
+            $moviment->voute = $params['voute'];
             $moviment->url_picture = $params['url_picture'];
 
             if (!$moviment->save()) {
@@ -43,11 +45,30 @@ class MovimentController extends Controller
     }
 
     public function get(Request $request) {
-        // $filters = $request->post();
+        $filters = $request->get('filters');
 
-        $moviments = Moviment::query()->get()->toArray();
+        if (!isset($filters['type']) || !$filters['type']) {
+            throw new Exception("Parameter type is required");
+        }
+        
+        //Não estava sendo possível usar o Where, estava sempre resultando nulo, analisar e mudar querys.
+        $response['sponsors'] = Moviment::whereRaw("type = 1")
+            ->limit(3)
+            ->get()
+            ->toArray();
 
-        $response['data'] = $moviments;
+        $response['votes'] = Moviment::whereRaw("type = 2")
+            ->limit(3)
+            ->get()
+            ->toArray();
+
+        $response['firstCounter'] = Moviment::selectRaw("id")
+            ->whereRaw('voute = "/lula"')
+            ->count();
+
+        $response['secondCounter'] = Moviment::selectRaw("id")
+            ->whereRaw('voute = "/bolsonaro"')
+            ->count();
 
         return response()->json($response);
     }
